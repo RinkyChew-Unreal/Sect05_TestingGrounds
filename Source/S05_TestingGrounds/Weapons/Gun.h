b49cf36f4926b6bfffbbbd7188ffd6bf7c291604
DESKTOP-AT9GER2\mmcel <mmcelt@yahoo.com>
2018-01-23 19:44:40 -0700

    253-Configuring the Gun

--- Source/S05_TestingGrounds/Weapons/Gun.h
+++ Source/S05_TestingGrounds/Weapons/Gun.h
@@ -37,15 +37,15 @@ public:
 
    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
    class UAnimInstance* AnimInstance;
+
 protected:
    // Called when the game starts or when spawned
    virtual void BeginPlay() override;
-
-   /** Fires a projectile. */
-   void OnFire();
 
 public:	
    // Called every frame
    virtual void Tick(float DeltaTime) override;
 
+   /** Fires a projectile. */
+   void OnFire();
 };
